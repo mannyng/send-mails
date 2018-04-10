41585e88baf7a3f17708dcf6d89ec636a6a125dd
rorturtola <emman.nwabueze@gmail.com>
2018-04-10 15:06:18 +0000

    delayed job and bootstrp buttons

--- app/controllers/massmails_controller.rb
+++ app/controllers/massmails_controller.rb
@@ -50,9 +50,10 @@ class MassmailsController < ApplicationController
     imagepath = params[:imagepath]
     linkpath = params[:linkpath]
     friends.each do |friend| 
-      SendLetterEmailJob.set(cron: '*/1 * * * *').perform_later(friend,toreply,data,subject,greeting,sender_name,sender)
-      #SendLetterEmailJob.set(wait: 40.seconds).perform_later(friend,toreply,data,subject,greeting,sender_name,sender)
+      #SendLetterEmailJob.set(cron: '*/1 * * * *').perform_later(friend,toreply,data,subject,greeting,sender_name,sender)
+      SendLetterEmailJob.set(wait: 40.seconds).perform_later(friend,toreply,data,subject,greeting,sender_name,sender)
     end
+    SendLetterEmailJob.set(cron: '*/1 * * * * RAILS_ENV=production bundle exec rake jobs:workoff').perform_later
     redirect_to root_path
   end 
   def show
